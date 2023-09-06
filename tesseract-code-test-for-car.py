8c5fa9c30ba59ef67249f1d7344a468012badd7a
Kanad-Patel <121385830+Kanad-Patel@users.noreply.github.com>
2023-09-07 01:58:28 +0530

    Update tesseract-code-test-for-car.py

--- tesseract-code-test-for-car.py
+++ tesseract-code-test-for-car.py
@@ -3,9 +3,6 @@ import cv2
 import pytesseract
 from pytesseract import Output
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-
 in1 = 24
 in2 = 23
 ena = 25
@@ -29,21 +26,34 @@ p=GPIO.PWM(ena,1000)
 p.start(50)
 q.start(50)
 
-while True():
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+ 
+while True:
+    # Capture frame-by-frame
     ret, frame = cap.read()
-    x = pytesseract.image_to_data(frame, output_type=Output.DICT)
-    text = len(x['text'])
-    for i in range(text):
-
-        if int(x['conf'][i]) > 60:
-
-            d = x['text'][i]
-
-            if d=='f':
-                print("forward")
+ 
+    d = pytesseract.image_to_data(frame, output_type=Output.DICT)
+    n_boxes = len(d['text'])
+    for i in range(n_boxes):
+        if int(d['conf'][i]) > 60:
+            (text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+            if(text == 'Forward'):
                 GPIO.output(in1,GPIO.HIGH)
                 GPIO.output(in2,GPIO.LOW)
                 GPIO.output(in3,GPIO.HIGH)
                 GPIO.output(in4,GPIO.LOW)
-            
-GPIO.cleanup()
+                print("forward")
+            # don't show empty text
+            if text and text.strip() != "":
+                frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                frame = cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
+ 
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+ 
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
